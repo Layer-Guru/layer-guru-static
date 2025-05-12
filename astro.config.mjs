7e5b9fe0ccdc6b1b5ba68518807760c5fc6825a8
Daniel Weiß <daniel.weiss@grollmus.de>
2025-05-12 14:37:12 +0200

    first changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://layer-guru.github.io",
+  base: "layer-guru-static",
+  devToolbar: {
+    enabled: true,
+  },
+  i18n: {
+    locales: ["de", "en"],
+    defaultLocale: "en",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
